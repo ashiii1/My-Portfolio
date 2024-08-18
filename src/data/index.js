71bbab237a60fedf2ad26b0536754a2a2db2bb73
ashiii1 <aashishaik526@gmail.com>
2024-08-18 16:40:37 +0530

    done

--- src/data/index.js
+++ src/data/index.js
@@ -9,7 +9,7 @@ export const personal_info = {
 
     ],
     github: "https://github.com/ashiii1",
-    cv: "https://drive.google.com/file/d/1eAv-x4Q5yMfzBWgZLzCyCaGi63nLhdEQ/view?usp=drive_link",
+    cv: "https://drive.google",
     linkedin: "https://www.linkedin.com/in/aashi526/",
     twitter:"https://x.com/_Aashii_1"
 };
