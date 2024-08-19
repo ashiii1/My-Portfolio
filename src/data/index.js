7dbeb467fe3d1c15baaf3d9b8ecd3f6212b61eb9
Sk Ashi <aashishaik526@gmail.com>
2024-08-19 16:31:55 +0530

    Update index.js

--- src/data/index.js
+++ src/data/index.js
@@ -9,7 +9,7 @@ export const personal_info = {
 
     ],
     github: "https://github.com/ashiii1",
-    cv: "https://drive.google.com/file/d/1T-YzDPp93tntwC7Q7rZT22InHOb6d5fH/view",
+    cv: "https://drive.google.com/file/d/1wSNVfx4IhKawyg6kzmQlGpYEHmXkHrCM/view?usp=drive_link",
     linkedin: "https://www.linkedin.com/in/aashi526/",
     twitter:"https://x.com/_Aashii_1"
 };
